c41e88744484aa0f378f6122522ee3c1fbe4feff
Damian Kliber <damiankliber@gmail.com>
2025-02-26 15:52:56 +0100

    feat: temporary redirect to my portfolio account

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,26 @@
-import { Box, Button, Heading, Text, VStack } from "@chakra-ui/react";
+"use client";
+
+import { Box, Spinner } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const Invitation = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/user/1");
+  }, [router]);
+
   return (
     <Box
       w="100vw"
       h="100vh"
-      bgGradient="linear(to-br, purple.600, pink.500, orange.400)"
       display="flex"
       alignItems="center"
       justifyContent="center"
-      pos="relative"
-      overflow="hidden"
+      bgGradient="linear(to-br, purple.600, pink.500, orange.400)"
     >
-      {/* Półprzezroczysta nakładka dla lepszego kontrastu */}
-      <Box
-        pos="absolute"
-        top={0}
-        left={0}
-        w="100%"
-        h="100%"
-        bg="rgba(0, 0, 0, 0.3)"
-        zIndex={1}
-      />
-      <VStack
-        p={10}
-        bg="whiteAlpha.900"
-        borderRadius="xl"
-        boxShadow="2xl"
-        zIndex={2}
-      >
-        <Heading as="h1" size="2xl" color="gray.800">
-          Stwórz swoje CV
-        </Heading>
-        <Text fontSize="lg" color="gray.600" textAlign="center">
-          Dołącz do naszej platformy i wyróżnij się na rynku pracy dzięki profesjonalnie przygotowanemu CV.
-        </Text>
-        <Button
-          // rightIcon={<ArrowForwardIcon />}
-          colorScheme="pink"
-          size="lg"
-          // onClick={() => router.push("/create-cv")}
-          _hover={{ transform: "scale(1.05)", bg: "pink.400" }}
-          transition="all 0.2s"
-        >
-          Rozpocznij teraz
-        </Button>
-      </VStack>
+      <Spinner size="xl" color="white" />
     </Box>
   );
 };
